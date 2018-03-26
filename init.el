28d6477defccf620c5c63f35df17a186f113d6dc
Angel ILIEV <a.v.iliev13@gmail.com>
2018-03-26 14:00:16 +0300

    Update init.el

--- init.el
+++ init.el
@@ -34,26 +34,4 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-default-notes-file (concat org-directory "/notes.org"))
- '(org-directory "~/orgfiles")
- '(org-export-html-postamble nil)
- '(org-hide-leading-stars t)
- '(org-startup-folded (quote overview))
- '(org-startup-indented t)
- '(package-selected-packages
-   (quote
-    (virtualenv flycheck which-key use-package try tabbar org-bullets matlab-mode eval-in-repl ess counsel color-theme auto-complete 2048-game))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
-
 ;;; init.el ends here
