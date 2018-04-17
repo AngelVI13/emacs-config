d1472204e2ecae0020c7dd4044ab9e54b8a8aad5
Angel ILIEV <a.v.iliev13@gmail.com>
2018-04-17 21:51:08 +0300

    Improvements, removed windmove, etc

--- init.el
+++ init.el
@@ -36,4 +36,27 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
 ;;; init.el ends here
-
+(put 'dired-find-alternate-file 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/orgfiles/gcal.org" "~/Dropbox/orgfiles/index.org")))
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Dropbox/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
+ '(package-selected-packages
+   (quote
+    (company dashboard diminish spaceline zenburn-theme wttrin which-key wgrep-ag web-mode virtualenv use-package try treemacs-projectile treemacs-evil tabbar solarized-theme smartparens smart-mode-line-powerline-theme real-auto-save pcre2el ox-reveal ox-pandoc org-gcal org-bullets org-ac noflet multiple-cursors move-text matlab-mode markdown-mode linum-relative key-chord jedi irony-eldoc iedit hungry-delete htmlize google-translate go-mode ggtags forecast flycheck expand-region eval-in-repl ess engine-mode emms elpy dumb-jump dracula-theme counsel-projectile company-irony color-theme chess bongo better-shell beacon all-the-icons aggressive-indent ag 2048-game))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
